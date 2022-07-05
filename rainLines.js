48ce73f813316bce63c019d5c7a0069d34721147
jonatan <jonatanmeza01@gmail.com>
2022-07-04 21:48:45 -0300

    Update project

--- rainLines.js
+++ rainLines.js
@@ -1,5 +1,5 @@
 // number of drops created.
-var nbDrop = 336;
+var nbDrop = 400;
 
 // function to generate a random number range.
 function randRange(minNum, maxNum) {
@@ -9,12 +9,13 @@ function randRange(minNum, maxNum) {
 // function to generate drops
 function createRain() {
   for (i = 1; i < nbDrop; i++) {
-    var dropLeft = randRange(0, 1600);
+    // var dropLeft = randRange(0, 1600);
+    var dropLeft = randRange(69, 1600);
     var dropTop = randRange(-1000, 1400);
 
     $(".rain").append('<div class="drop" id="drop' + i + '"></div>');
     $("#drop" + i).css("left", dropLeft);
-    $("#drop" + i).css("top", dropTop);
+    $("#drop" + i).css("bottom", dropTop);
   }
 }
 // Make it rain
